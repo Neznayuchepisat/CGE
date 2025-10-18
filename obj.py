e73ab63235614c84fd8ddcec9e75e6e60ab5582e
Neznayuchepisat <nikita.p.myagkikh12@gmail.com>
2025-10-18 20:36:03 +0300

    obj.py обновлен, добавлены новые методы

--- obj.py
+++ obj.py
@@ -11,3 +11,11 @@ class Object:
             ry = self.y + py
             if 0 <= rx < renderer.width and 0 <= ry < renderer.height:
                 renderer.map[ry][rx] = self.char
+
+    def __repr__(self):
+        return f"<Object '{self.char}' at ({self.x},{self.y})>"
+    
+    def iscollide(self, other):
+        self_pix = {(self.x + px, self.y + py) for px, py in self.pixels}
+        other_pix = {(other.x + px, other.y + py) for px, py in self.pixels}
+        return self_pix.isdisjoint(other_pix)
